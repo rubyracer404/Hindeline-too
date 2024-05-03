1e1aa9bf33923d186b98a144d528bbaacd01ba02
Opilane TTHK <150146511+rubyracer404@users.noreply.github.com>
2024-05-03 13:03:48 +0300

    testimine

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -5,6 +5,14 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            if (true)
+            {
+                Console.WriteLine("fent");
+                if (false)
+                {
+                    Console.WriteLine("orange box");
+                } else Console.WriteLine("aiaiaiia");
+            } else Console.WriteLine("my summer car");
         }
     }
 }
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int k = 0; k < 4; k++)
+            {
+                Console.WriteLine(k);
+            }
+        }
+    }
+}
